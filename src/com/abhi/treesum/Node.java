4503f1950cf985e0b756068b60f3ad96cd1e8ada
AbhiroopNigote11 <106870329+AbhiroopNigote11@users.noreply.github.com>
2022-07-25 23:01:45 +0530

    Add files via upload

---
+++ src/com/abhi/treesum/Node.java
@@ -0,0 +1,11 @@
+package com.abhi.treesum;
+
+public class Node {
+       int nodeData;
+       Node leftNode,rightNode;
+       
+       Node(int data){
+    	   nodeData=data;
+    	   leftNode = rightNode= null;
+       }
+} 
